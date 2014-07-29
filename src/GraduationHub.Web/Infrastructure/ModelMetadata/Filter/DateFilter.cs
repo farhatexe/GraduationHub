5c182c3202e6b2532fb97e54deeba0ae80a3027d
Chuck Bryan <swampyfox@gmail.com>
2014-07-28 22:15:07 -0400

    Missed these two files

---
+++ src/GraduationHub.Web/Infrastructure/ModelMetadata/Filter/DateFilter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GraduationHub.Web.Infrastructure.ModelMetadata.Filter
+{
+    // see: http://bit.ly/1o80jkF for explanation
+    public class DateFilter : IModelMetadataFilter
+    {
+        public void TransformMetadata(System.Web.Mvc.ModelMetadata metadata, IEnumerable<Attribute> attributes)
+        {
+            if (metadata.ModelType.Name == "DateTime" &&
+                string.IsNullOrEmpty(metadata.DisplayFormatString))
+            {
+                metadata.DisplayFormatString = "{0:yyyy-MM-dd}";
+            }
+
+            if (metadata.ModelType.Name == "DateTime" &&
+                string.IsNullOrEmpty(metadata.EditFormatString))
+            {
+                metadata.EditFormatString = "{0:yyyy-MM-dd}";
+            }
+        }
+    }
+}
