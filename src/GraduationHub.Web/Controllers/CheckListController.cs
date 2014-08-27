7c7b5705a235cd609f67ecc610c68143810c12a1
Chuck Bryan <swampyfox@gmail.com>
2014-08-27 19:57:53 -0400

    styling up the pagses

---
+++ src/GraduationHub.Web/Controllers/CheckListController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GraduationHub.Web.Controllers
+{
+    public class CheckListController : Controller
+    {
+        public ActionResult Biography()
+        {
+            return View();
+        }
+
+        public ActionResult ExpressionOfThanks()
+        {
+            return View();
+        }
+
+        public ActionResult SlideShowCaption()
+        {
+            return View();
+        }
+
+        public ActionResult SeniorPortrait()
+        {
+            return View();
+        }
+
+        public ActionResult BabyPicture()
+        {
+            return View();
+        }
+
+        public ActionResult FirstYouthfulPicture()
+        {
+            return View();
+        }
+
+        public ActionResult SecondYouthfulPicture()
+        {
+            return View();
+        }
+        
+        public ActionResult ThirdYouthfulPicture()
+        {
+            return View();
+        }
+
+        public ActionResult FourthYouthfulPicture()
+        {
+            return View();
+        }
+    }
+}
